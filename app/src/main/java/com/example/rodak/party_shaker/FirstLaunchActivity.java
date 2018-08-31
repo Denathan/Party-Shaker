6301b0cdc1f659b97d44306446674af928ca5123
Daniel <danielrodak27@gmail.com>
2018-08-31 13:51:38 +0200

    First commit of the app.
    Created two first activities:
    -FirstLaunch
    -MainMenu
    
    No logic, only UI.

---
+++ app/src/main/java/com/example/rodak/party_shaker/FirstLaunchActivity.java
@@ -0,0 +1,13 @@
+package com.example.rodak.party_shaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstLaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_launch);
+    }
+}
---
+++ app/src/main/java/com/example/rodak/party_shaker/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.rodak.party_shaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
